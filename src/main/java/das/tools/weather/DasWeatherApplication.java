5b32968442aec8520c9093a1b5e1f2cc3146508b
Anry Das <anry.das@gmail.com>
2023-12-14 16:32:25 +0200

    Added Exit Application confirmation.

--- src/main/java/das/tools/weather/DasWeatherApplication.java
+++ src/main/java/das/tools/weather/DasWeatherApplication.java
@@ -1,10 +1,15 @@
 package das.tools.weather;
 
 import das.tools.weather.config.GuiConfig;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
@@ -13,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
+import java.util.Optional;
 
 @SpringBootApplication
 @Lazy
@@ -21,6 +27,8 @@ import java.util.Objects;
 public class DasWeatherApplication extends AbstractJavaFxApplicationSupport {
     @Autowired
     private GuiConfig.ViewHolder guiView;
+    @Value("${app.confirm-exit}")
+    private boolean isConfirmExit;
 
     @Override
     public void start(Stage stage) {
@@ -29,6 +37,21 @@ public class DasWeatherApplication extends AbstractJavaFxApplicationSupport {
         stage.setTitle("Das Weather");
         stage.setResizable(false);
         stage.setScene(scene);
+        if (isConfirmExit) {
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                            "Do you really want exit?");
+                    alert.setTitle("Confirm");
+                    alert.setHeaderText("Das Weather Application");
+                    Optional<ButtonType> option = alert.showAndWait();
+                    if (ButtonType.CANCEL.equals(option.orElse(null))) {
+                        event.consume();
+                    }
+                }
+            });
+        }
         stage.show();
     }
 
