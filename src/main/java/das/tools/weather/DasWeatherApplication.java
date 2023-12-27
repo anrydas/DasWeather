3d6de2a3d4e2531834a2a30d4cbbfa7991f99397
Anry Das <anry.d.as@gmail.com>
2023-12-27 10:20:07 +0200

    Code cleaning up

--- src/main/java/das/tools/weather/DasWeatherApplication.java
+++ src/main/java/das/tools/weather/DasWeatherApplication.java
@@ -10,7 +10,6 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
--- src/main/java/das/tools/weather/gui/ConfigController.java
+++ src/main/java/das/tools/weather/gui/ConfigController.java
@@ -3,8 +3,8 @@ package das.tools.weather.gui;
 import java.util.regex.Pattern;
 
 public interface ConfigController {
-    public static final Pattern FORECAST_URL_PATTERN = Pattern.compile("http\\:\\/\\/api\\.weatherapi\\.com\\/v1\\/forecast\\.json");
-    public static final Pattern API_KEY_PATTERN = Pattern.compile("^[a-fA-F0-9]+$");
+    Pattern FORECAST_URL_PATTERN = Pattern.compile("http\\:\\/\\/api\\.weatherapi\\.com\\/v1\\/forecast\\.json");
+    Pattern API_KEY_PATTERN = Pattern.compile("^[a-fA-F0-9]+$");
     void onShowingStage();
 
     boolean isConfigChanged();
