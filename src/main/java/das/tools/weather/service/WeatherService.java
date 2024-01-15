1d0dabcd5d9ee3c47ce081c1b31c07dceabb024d
Anry Das <anry.das@gmail.com>
2024-01-15 12:33:24 +0200

    Added weather condition's images to reduce traffic and improve performance

--- src/main/java/das/tools/weather/service/WeatherService.java
+++ src/main/java/das/tools/weather/service/WeatherService.java
@@ -4,11 +4,17 @@ import das.tools.weather.entity.CurrenWeatherResponse;
 import das.tools.weather.entity.ForecastWeatherResponse;
 import javafx.scene.image.Image;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public interface WeatherService {
+    Map<Integer,Integer> WEATHER_CODE_CONDITION_IMAGES = new HashMap<>(48);
     CurrenWeatherResponse getCurrentWeather();
 
     ForecastWeatherResponse getForecastWeather();
 
     Image getRemoteImage(String imageUrl);
+
+    Image getWeatherIcon(int weatherCode, boolean isDay);
 }
