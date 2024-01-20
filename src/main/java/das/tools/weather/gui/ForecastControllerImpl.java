a90810938022f5ce7070ad58d2c568c733d60d9e
Anry Das <anry.d.as@gmail.com>
2024-01-20 12:44:08 +0200

    Removed unnecessary method call

--- src/main/java/das/tools/weather/gui/ForecastControllerImpl.java
+++ src/main/java/das/tools/weather/gui/ForecastControllerImpl.java
@@ -136,7 +136,6 @@ public class ForecastControllerImpl implements ForecastController {
     @Override
     public void onShowing() {
         setTabNames();
-        fillGraphics();
     }
 
     private void setTabNames() {
