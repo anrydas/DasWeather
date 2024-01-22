8191b5ccfbda71690140df5815c7ecf306b8e005
Anry Das <anry.das@gmail.com>
2024-01-22 14:12:15 +0200

    Implemented new update weather job

--- src/main/java/das/tools/weather/DasWeatherApplication.java
+++ src/main/java/das/tools/weather/DasWeatherApplication.java
@@ -11,9 +11,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Timer;
@@ -21,10 +25,17 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static org.quartz.CronScheduleBuilder.cronSchedule;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+@Slf4j
 public class DasWeatherApplication extends Application {
-    public static final String APP_VERSION = "3.3.6-RELEASE";
+    public static final String APP_VERSION = "3.3.7-RELEASE";
     private final LocalizeResourcesService localizeService = LocalizeResourcesServiceImpl.getInstance();
-    private ScheduledExecutorService scheduler;
+    //private ScheduledExecutorService scheduler;
+    private Scheduler scheduler;
 
     @Override
     public void start(Stage stage) {
@@ -47,6 +58,11 @@ public class DasWeatherApplication extends Application {
                         return;
                     }
                 }
+                try {
+                    scheduler.shutdown(true);
+                } catch (SchedulerException e) {
+                    log.error("Error shutting down the scheduler: {}", e.getLocalizedMessage());
+                }
                 Platform.exit();
                 System.exit(0);
             });
@@ -58,8 +74,29 @@ public class DasWeatherApplication extends Application {
             throw new RuntimeException(e);
         }
 
-        scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(UpdateWeatherController.getInstance(), 5000, 600000, TimeUnit.MILLISECONDS);
+        scheduleUpdate();
+    }
+
+    private void scheduleUpdate() {
+        try {
+            SchedulerFactory sf = new StdSchedulerFactory();
+            scheduler = sf.getScheduler();
+            Date startTime = DateBuilder.nextGivenSecondDate(null, 10);
+            JobDetail job = newJob(UpdateWeatherController.class)
+                    .withIdentity("WeatherUpdate", "group1")
+                    .build();
+            Trigger trigger = newTrigger()
+                    .withIdentity("UpdateTrigger", "group1")
+                    .startAt(startTime)
+                    .withSchedule(
+                            simpleSchedule().withIntervalInSeconds(20).repeatForever()
+                    )
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+            scheduler.start();
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
