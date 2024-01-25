895a5b411935deffce3f5688aa566349d23b9494
Anry Das <anry.das@gmail.com>
2024-01-25 09:18:50 +0200

    Corrected Sun Tab

--- src/main/java/das/tools/weather/gui/ForecastController.java
+++ src/main/java/das/tools/weather/gui/ForecastController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public interface ForecastController extends Localized {
     Map<Integer,String> TAB_NAMES = new LinkedHashMap<>();
     Map<String,String> FILE_FORMAT_NAMES = new HashMap<>();
-    double MINUTES_TO_DECIMAL_FACTOR = 0.167;
+    double MINUTES_TO_DECIMAL_FACTOR = 0.0167;
 
     void onShowing();
 
