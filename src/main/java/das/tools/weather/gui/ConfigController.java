be6bef877876306877d92b2bd5a551810db9580d
Anry Das <anry.das@gmail.com>
2024-02-06 09:52:47 +0200

    Using FxWeaver. Added possibility to show multiple Forecast windows.

--- src/main/java/das/tools/weather/gui/ConfigController.java
+++ src/main/java/das/tools/weather/gui/ConfigController.java
@@ -1,10 +1,18 @@
 package das.tools.weather.gui;
 
+import javafx.scene.image.Image;
+
 import java.util.regex.Pattern;
 
 public interface ConfigController extends Localized {
     Pattern FORECAST_URL_PATTERN = Pattern.compile("http\\:\\/\\/api\\.weatherapi\\.com\\/v1\\/forecast\\.json");
     Pattern API_KEY_PATTERN = Pattern.compile("^[a-fA-F0-9]+$");
+
+    void show();
+
+
+    void setWindowIcon(Image icon);
+
     void onShowingStage();
 
     void setLocationConfirmation();
--- src/main/java/das/tools/weather/gui/LocationControllerImpl.java
+++ src/main/java/das/tools/weather/gui/LocationControllerImpl.java
@@ -6,28 +6,37 @@ import das.tools.weather.service.LocalizeResourcesService;
 import das.tools.weather.service.WeatherService;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Properties;
 
 @Component
+@FxmlView("/fxml/Location.fxml")
 @Slf4j
 public class LocationControllerImpl implements LocationController {
-    @Autowired private LocalizeResourcesService localizeService;
-    @Autowired private WeatherService weatherService;
-    @Autowired private GuiConfigService configService;
+    private final LocalizeResourcesService localizeService;
+    private final WeatherService weatherService;
+    private final GuiConfigService configService;
+    private final BuildProperties buildProperties;
     private WeatherLocation[] foundLocations;
     private String key;
 
+    @FXML private AnchorPane root;
     @FXML private Label lbLocationName;
     @FXML private Label lbLocationsList;
     @FXML private TextField edLocationName;
@@ -35,9 +44,20 @@ public class LocationControllerImpl implements LocationController {
     @FXML private ListView<String> lstLocations;
     @FXML private Button btOk;
     @FXML private Button btCancel;
+    private Stage stage;
+
+    public LocationControllerImpl(LocalizeResourcesService localizeService, WeatherService weatherService, GuiConfigService configService, BuildProperties buildProperties) {
+        this.localizeService = localizeService;
+        this.weatherService = weatherService;
+        this.configService = configService;
+        this.buildProperties = buildProperties;
+    }
 
     @FXML
     private void initialize() {
+        this.stage = new Stage();
+        stage.setScene(new Scene(root));
+
         edLocationName.setOnKeyReleased(keyEvent -> locationNamePressed());
         btSearch.setOnAction(actionEvent -> searchForLocations());
         btOk.setOnAction(actionEvent -> saveLocation());
@@ -84,8 +104,24 @@ public class LocationControllerImpl implements LocationController {
     }
 
     @Override
+    public void show() {
+        stage.setTitle(String.format("Das Weather Location (v.%s)", buildProperties.getVersion()));
+
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        initLocale();
+        stage.showAndWait();
+    }
+
+    @Override
     public void setApiKey(String key) {
         this.key = key;
+    }
+
+    @Override
+    public void setWindowIcon(Image icon) {
+        stage.getIcons().clear();
+        stage.getIcons().add(icon);
     }
 
     private void searchForLocations() {
