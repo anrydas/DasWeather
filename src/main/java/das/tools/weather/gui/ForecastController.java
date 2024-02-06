3e3d5938970ec9c7e3d7b4ca53143f571c04ba2a
Anry Das <anry.das@gmail.com>
2024-02-06 12:45:25 +0200

    Moved show() method to separate interface

--- src/main/java/das/tools/weather/gui/ForecastController.java
+++ src/main/java/das/tools/weather/gui/ForecastController.java
@@ -7,12 +7,10 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public interface ForecastController extends Localized {
+public interface ForecastController extends Localized, GuiWindow {
     Map<Integer,String> TAB_NAMES = new LinkedHashMap<>();
     Map<String,String> FILE_FORMAT_NAMES = new HashMap<>();
     double MINUTES_TO_DECIMAL_FACTOR = 0.0167;
-
-    void show();
 
     void setWindowIcon(Image icon);
 
