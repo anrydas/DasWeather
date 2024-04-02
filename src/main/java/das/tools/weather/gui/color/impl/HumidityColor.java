cac1d016dd1e1d4bea15f22fb6eee076b5925259
Anry Das <anry.das@gmail.com>
2024-04-02 12:36:02 +0300

    Humidity Color correction

--- src/main/java/das/tools/weather/gui/color/impl/HumidityColor.java
+++ src/main/java/das/tools/weather/gui/color/impl/HumidityColor.java
@@ -13,6 +13,7 @@ public class HumidityColor extends AbstractColor implements ColorEngine {
         COLOR_MAP.put(0, "#FFFFBC");
         COLOR_MAP.put(40, "#A3FFB9");
         COLOR_MAP.put(60, "#7BF3F5");
+        COLOR_MAP.put(70, "#7BD4F5");
         COLOR_MAP.put(80, "#7B88F5");
     }
     @Override
